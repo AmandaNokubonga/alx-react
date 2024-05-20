a96662daf75e7884034f63e1e1529e3eeacfeecb
AmandaNokubonga <amandamagwaza3@gmail.com>
2024-05-20 17:07:39 +0000

    Resolved CSS conflicts and improved responsiveness

---
+++ 0x00-Webpack/task_3/modules/footer/footer.js
@@ -0,0 +1,7 @@
+const $ = require( "jquery" );
+import "./footer.css";
+
+$('body').append("<p>Copyright - Holberton School</p>");const $ = require( "jquery" );
+import "./footer.css";
+
+$('body').append("<p>Copyright - Holberton School</p>");
