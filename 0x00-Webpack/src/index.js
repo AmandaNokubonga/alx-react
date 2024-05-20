8a2de149572bd5ac50164cdc9fa574c562ca6af6
AmandaNokubonga <amandamagwaza3@gmail.com>
2024-05-20 16:26:13 +0000

    filingalx

---
+++ 0x00-Webpack/src/index.js
@@ -0,0 +1,9 @@
+const $ = require( "jquery" );
+
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<p>Copyright - Holberton School</p>');const $ = require( "jquery" );
+
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<p>Copyright - Holberton School</p>');
