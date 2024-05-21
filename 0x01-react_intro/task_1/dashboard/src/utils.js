4e48381f99778ca7b4b3df67f288bafc46bf5fd1
AmandaNokubonga <amandamagwaza3@gmail.com>
2024-05-21 18:06:24 +0000

    eturns the current year

---
+++ 0x01-react_intro/task_1/dashboard/src/utils.js
@@ -0,0 +1,19 @@
+export function getFullYear() {
+  return new Date().getFullYear();
+}
+
+export function getFooterCopy(isIndex) {
+  if (isIndex) {
+    return 'Holberton School';
+  }
+  return 'Holberton School main dashboard';
+}export function getFullYear() {
+  return new Date().getFullYear();
+}
+
+export function getFooterCopy(isIndex) {
+  if (isIndex) {
+    return 'Holberton School';
+  }
+  return 'Holberton School main dashboard';
+}
